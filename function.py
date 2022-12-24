29434369b357a3aac297245b8c1e069ad324019f
kaskang99 <moonsukang99@gmail.com>
2022-12-24 11:43:16 -0300

    v2 of project
    Change from python to JS
    Still only works with preset lists of names

--- function.py
+++ function.py
@@ -1,12 +1,11 @@
 import random as random
 
 list_names = ["kang","jenny","antonio","cristina","davi","solomon","jay","jenny","stella","renan","erick","jung sun noh","juliana"]
-
-def choose_name(list):
-    nome = input("escreva seu nome: ")
+nome = input("qual seu nome: ")
+def choose_name(list,name):
     for i in range(len(list_names)):
         chosen = random.choice(list_names)
-        if chosen != nome:
+        if chosen != name:
             print("Compre um presente para: " + chosen)
             list_names.remove(chosen)
             print(list_names)
@@ -15,4 +14,4 @@ def choose_name(list):
             chosen = random.choice(list_names)
         return chosen
 
-print(choose_name(list_names))
+print(choose_name(list_names,nome))
