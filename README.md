6ec3a95bc888992f4ce6730575443723fb131247
kaskang99 <moonsukang99@gmail.com>
2022-12-24 00:33:56 -0300

    home page done
    Still needs the actual function to be implemented in js instead of python

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Secret_Santa
+# Secret Santa - by Lucas Kang
+---
+After being fed up of looking for a website or app that doesn't require personal information (name, e-mail, phone number, etc), I decided to build my own.
+
+The objective of this project is to create a wesite in GitHub Pages that allows you to create a list of participants and then allow each of them to enter their name and see who they picked to gift.
+
+Current state of the project:
+
+* uses a pre-defined list
+* allows input of name
+* no working website
+* no HTML
+* no CSS
+* no Python
+* no Python framework
---
+++ function.py
@@ -0,0 +1,18 @@
+import random as random
+
+list_names = ["kang","jenny","antonio","cristina","davi","solomon","jay","jenny","stella","renan","erick","jung sun noh","juliana"]
+
+def choose_name(list):
+    nome = input("escreva seu nome: ")
+    for i in range(len(list_names)):
+        chosen = random.choice(list_names)
+        if chosen != nome:
+            print("Compre um presente para: " + chosen)
+            list_names.remove(chosen)
+            print(list_names)
+            break
+        else:
+            chosen = random.choice(list_names)
+        return chosen
+
+print(choose_name(list_names))
